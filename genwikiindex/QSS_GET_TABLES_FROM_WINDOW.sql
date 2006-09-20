32d29ca3ed5b1eb060a28a9bfeca2bc7a2669097
Carlos Ruiz <carg67@gmail.com>
2006-09-20 16:17:14 +0000

    Scripts to generate wiki index manual page

---
+++ genwikiindex/QSS_GET_TABLES_FROM_WINDOW.sql
@@ -0,0 +1,36 @@
+CREATE OR REPLACE FUNCTION qss_get_tables_from_window (p_ad_window_id IN NUMBER)
+   RETURN VARCHAR2
+AS
+/*
+/*
+# ADempiere contribution
+# Author: Carlos Antonio Ruiz Gomez - globalqss
+# Function to obtain in a query all tables maintained by option
+*/
+   v_tables   VARCHAR2 (2000);
+BEGIN
+   v_tables := RPAD (' ', 2000, ' ');
+   v_tables := '';
+   FOR r IN (SELECT   ad_tab_id,
+                      RPAD ('+', (tablevel), '+') || tablename tablename
+                 FROM ad_tab b, ad_table t
+                WHERE b.ad_table_id = t.ad_table_id
+                  AND b.ad_window_id = p_ad_window_id
+             ORDER BY seqno)
+   LOOP
+      v_tables := v_tables || ' ' || r.tablename;
+      FOR j IN (SELECT NVL (p.classname, p.procedurename) cmd
+                  FROM ad_field f, ad_column c, ad_process p
+                 WHERE f.ad_tab_id = r.ad_tab_id
+                   AND f.ad_column_id = c.ad_column_id
+                   AND c.ad_reference_id = 28
+                   AND c.ad_process_id = p.ad_process_id)
+      LOOP
+         IF j.cmd IS NOT NULL
+         THEN
+            v_tables := v_tables || ' (' || j.cmd || ')';
+         END IF;
+      END LOOP;
+   END LOOP;
+   RETURN SUBSTR (v_tables, 2);
+END;
---
+++ genwikiindex/genWiki_lang.sql
@@ -0,0 +1,93 @@
+/*
+# ADempiere contribution
+# Author: Carlos Antonio Ruiz Gomez - globalqss
+# Generates wiki page from the menu tree
+# for system languages
+# You must change the two lines marked as:
+                      -- Change it here for your language !!!!
+#
+# Prerequisite: create first the function qss_get_tables_from_window
+#
+*/
+SELECT     CASE
+              WHEN LEVEL = 1 AND issummary = 'Y'
+                 THEN '== ' || mt.NAME || ' (' || m.NAME || ') =='
+              WHEN LEVEL = 2 AND issummary = 'Y'
+                 THEN '=== ' || mt.NAME || ' (' || m.NAME || ') ==='
+              WHEN LEVEL = 3 AND issummary = 'Y'
+                 THEN '==== ' || mt.NAME || ' (' || m.NAME || ') ===='
+              WHEN LEVEL = 4 AND issummary = 'Y'
+                 THEN '===== ' || mt.NAME || ' (' || m.NAME || ') ====='
+              WHEN issummary != 'Y'
+              AND NVL
+                     (f.classname,
+                      NVL
+                         (p.classname,
+                          NVL (p.procedurename,
+                               NVL (k.os_command,
+                                    qss_get_tables_from_window (m.ad_window_id)
+                                   )
+                              )
+                         )
+                     ) IS NOT NULL
+                 THEN    '* [[ManPage'
+                      || m.action
+                      || '_'
+                      || REPLACE (REPLACE (m.NAME, ' ', ''), '_', '')
+                      -- Change it here for your language !!!!
+                      || '_es|'
+                      || mt.NAME
+                      || ']] ('
+                      || m.NAME
+                      || ') <span style="font-variant:small-caps">'
+                      || DECODE (m.action,
+                                 'B', 'Workbench',
+                                 'F', 'WorkFlow',
+                                 'P', 'Process',
+                                 'R', 'Report',
+                                 'T', 'Task',
+                                 'W', 'Window',
+                                 'X', 'Form',
+                                 '', 'Menu',
+                                 '?'
+                                )
+                      || '</span>'
+                      || '''''   ->   ('
+                      || NVL
+                            (f.classname,
+                             NVL
+                                (p.classname,
+                                 NVL
+                                    (p.procedurename,
+                                     NVL
+                                        (k.os_command,
+                                         qss_get_tables_from_window
+                                                               (m.ad_window_id)
+                                        )
+                                    )
+                                )
+                            )
+                      || ')'''''
+              ELSE    '<!--'
+                   || mt.NAME
+                   || ' / '
+                   || m.NAME
+                   || ' - this option has not associated command -->'
+           END CASE
+      FROM ad_treenodemm t,
+           ad_menu m,
+           ad_menu_trl mt,
+           ad_process p,
+           ad_form f,
+           ad_task k
+     WHERE t.ad_tree_id = 10
+       AND t.node_id = m.ad_menu_id
+       -- Change it here for your language !!!!
+       AND mt.ad_language(+) = 'es_MX'
+       AND mt.ad_menu_id(+) = m.ad_menu_id
+       AND m.ad_process_id = p.ad_process_id(+)
+       AND m.ad_form_id = f.ad_form_id(+)
+       AND m.ad_task_id = k.ad_task_id(+)
+CONNECT BY NOCYCLE PRIOR t.node_id = t.parent_id
+START WITH t.parent_id = 0 OR parent_id IS NULL
+  ORDER SIBLINGS BY t.seqno;
