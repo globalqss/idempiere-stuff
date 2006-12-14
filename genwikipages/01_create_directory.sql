87d0f7f9a640a3d521aaf94ab71cb89919357474
globalqss <globalqss@localhost>
2006-12-14 16:46:04 +0000

    Generating manual for 3.1.2

--- genwikipages/01_create_directory.sql
+++ genwikipages/01_create_directory.sql
@@ -1,1 +1,10 @@
 CREATE OR REPLACE DIRECTORY dir_temp AS '/tmp/ADEMPIERE_MANUAL';
+
+/*
+Remember also that you must give permissions
+
+sqlplus sys/password as sysdba
+
+grant execute on utl_file to adempiere;
+
+*/
