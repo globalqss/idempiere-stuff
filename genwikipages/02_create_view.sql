18d05573c04459865f6d3374f6e7f7d8cf0eee3e
afalcone <afalcone@localhost>
2007-06-01 13:49:14 +0000

    Added tabs when they are based in views

--- genwikipages/02_create_view.sql
+++ genwikipages/02_create_view.sql
@@ -1,10 +1,13 @@
-CREATE OR REPLACE FORCE VIEW rv_query_for_manual
-/**********************************************************************************************
+
+CREATE OR REPLACE FORCE VIEW rv_query_for_manual 
+  
+**********************************************************************************************
 /* REM VIEW FOR MANUAL: THIS VIEW MUST BE CREATED BEFORE EXECUTE THE SCRIPT !.
    Contributor: Carlos Antonio Ruiz Gomez - globalqss
 */
 AS
-   SELECT vt.ad_language, vt.ad_window_id, 0 ad_tab_id, 0 ad_field_id, 'W' TYPE,
+
+  SELECT vt.ad_language, vt.ad_window_id, 0 ad_tab_id, 0 ad_field_id, 'W' TYPE,
           vt.NAME, vt.description, vt.HELP, 0 seqtab, 0 seqfld, '' dbtable,
           '' dbcolumn, '' dbtype, '' adempieretype, w.ISBETAFUNCTIONALITY
      FROM ad_window_vt vt
@@ -27,6 +30,19 @@ AS
                 AND entitytype = 'D')
       AND t.ad_table_id = tt.ad_table_id
       AND UPPER (tt.tablename) = ut.table_name
+   UNION
+-- When are views
+   SELECT t.ad_language, t.ad_window_id, t.ad_tab_id, 0, 'T', t.NAME,
+          t.description, t.HELP, t.seqno, 0, tt.tablename, '', '', '', ''
+     FROM ad_tab_vt t, ad_table tt, user_views uv
+    WHERE EXISTS (
+             SELECT 1
+               FROM ad_tab
+              WHERE ad_tab.ad_tab_id = t.ad_tab_id
+                AND isactive = 'Y'
+                AND entitytype = 'D')
+      AND t.ad_table_id = tt.ad_table_id
+      AND UPPER (tt.tablename) = uv.view_name
    UNION
    SELECT f.ad_language, t.ad_window_id, t.ad_tab_id, f.ad_field_id, 'F',
           f.NAME, f.description, f.HELP, t.seqno, f.seqno, tt.tablename,
@@ -68,6 +84,48 @@ AS
       AND f.ad_column_id = cc.ad_column_id
       AND UPPER (tt.tablename) = uc.table_name
       AND UPPER (cc.columnname) = uc.column_name
+UNION
+-- When are views      
+   SELECT f.ad_language, t.ad_window_id, t.ad_tab_id, f.ad_field_id, 'F',
+          f.NAME, f.description, f.HELP, t.seqno, f.seqno, tt.tablename,
+          cc.columnname,
+             uc.data_type
+          || DECODE (uc.data_type,
+                     'NVARCHAR2', '(' || uc.char_length || ')',
+                     'CHAR', '(' || uc.char_length || ')',
+                     'NUMBER', DECODE (uc.data_precision,
+                                       NULL, NULL,
+                                          '('
+                                       || uc.data_precision
+                                       || DECODE (uc.data_scale,
+                                                  0, NULL,
+                                                  ',' || uc.data_precision
+                                                 )
+                                       || ')'
+                                      )
+                    ),
+          dba_displaytype (cc.ad_reference_id),
+	  ''
+     FROM ad_field_vt f,
+          ad_tab t,
+          ad_table tt,
+          user_views uv,
+          ad_column cc,
+          user_tab_columns uc
+    WHERE f.ad_tab_id = t.ad_tab_id
+      AND isdisplayed = 'Y'
+      AND EXISTS (
+             SELECT 1
+               FROM ad_field
+              WHERE ad_field.ad_tab_id = f.ad_tab_id
+                AND isactive = 'Y'
+                AND entitytype = 'D')
+      AND t.isactive = 'Y'
+      AND t.ad_table_id = tt.ad_table_id
+      AND UPPER (tt.tablename) = uv.view_name
+      AND f.ad_column_id = cc.ad_column_id
+      AND UPPER (tt.tablename) = uc.table_name
+      AND UPPER (cc.columnname) = uc.column_name
    UNION
 -- Base Language en_US
    SELECT 'en_US_base', ad_window_id, 0 ad_tab_id, 0 ad_field_id, 'W' TYPE,
@@ -82,6 +140,15 @@ AS
     WHERE t.isactive = 'Y'
       AND t.ad_table_id = tt.ad_table_id
       AND UPPER (tt.tablename) = ut.table_name
+      AND t.entitytype = 'D'
+   UNION
+-- When are views
+   SELECT 'en_US_base', t.ad_window_id, t.ad_tab_id, 0, 'T', t.NAME,
+          t.description, t.HELP, t.seqno, 0, tt.tablename, '', '', '',''
+     FROM ad_tab t, ad_table tt, user_views uv
+    WHERE t.isactive = 'Y'
+      AND t.ad_table_id = tt.ad_table_id
+      AND UPPER (tt.tablename) = uv.view_name
       AND t.entitytype = 'D'
    UNION
    SELECT 'en_US_base', t.ad_window_id, t.ad_tab_id, f.ad_field_id, 'F',
@@ -119,3 +186,41 @@ AS
       AND UPPER (tt.tablename) = uc.table_name
       AND UPPER (cc.columnname) = uc.column_name
       AND f.entitytype = 'D'
+   UNION
+-- When are views
+   SELECT 'en_US_base', t.ad_window_id, t.ad_tab_id, f.ad_field_id, 'F',
+          f.NAME, f.description, f.HELP, t.seqno, f.seqno, tt.tablename,
+          cc.columnname,
+             uc.data_type
+          || DECODE (uc.data_type,
+                     'NVARCHAR2', '(' || uc.char_length || ')',
+                     'CHAR', '(' || uc.char_length || ')',
+                     'NUMBER', DECODE (uc.data_precision,
+                                       NULL, NULL,
+                                          '('
+                                       || uc.data_precision
+                                       || DECODE (uc.data_scale,
+                                                  0, NULL,
+                                                  ',' || uc.data_precision
+                                                 )
+                                       || ')'
+                                      )
+                    ),
+          dba_displaytype (cc.ad_reference_id),''
+     FROM ad_field f,
+          ad_tab t,
+          ad_table tt,
+          user_views uv,
+          ad_column cc,
+          user_tab_columns uc
+    WHERE f.ad_tab_id = t.ad_tab_id
+      AND isdisplayed = 'Y'
+      AND f.isactive = 'Y'
+      AND t.isactive = 'Y'
+      AND t.ad_table_id = tt.ad_table_id
+      AND UPPER (tt.tablename) = uv.view_name
+      AND f.ad_column_id = cc.ad_column_id
+      AND UPPER (tt.tablename) = uc.table_name
+      AND UPPER (cc.columnname) = uc.column_name
+      AND f.entitytype = 'D'
+ 
