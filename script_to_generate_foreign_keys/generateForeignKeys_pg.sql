851bf952eccceca6d4ce80e4db3d26e64c32119d
Carlos Ruiz <carg67@gmail.com>
2009-11-04 18:00:56 +0000

    Contribute foreign key generator to be run on postgresql db Implement the DEFERRABLE INITIALLY DEFERRED suggestion from Heng Sin

--- script_to_generate_foreign_keys/generateForeignKeys_pg.sql
+++ script_to_generate_foreign_keys/generateForeignKeys_pg.sql
@@ -3,16 +3,15 @@
  # Author: Carlos Ruiz - globalqss
  # This script generates the alter table commands to create the foreign keys 
  # based on Adempiere Dictionary definition
- # It runs in oracle database - to generate commands for postgresql comment the lines marked with "for oracle"
- # and comment out the lines marked "for postgresql"
- # 
+ # It runs in postgresql database - to generate commands for oracle comment the lines marked with "for postgresql"
+ # and comment out the lines marked "for oracle"
 */
 
 SELECT      'ALTER TABLE '
          || tablename
-         || ' ADD (CONSTRAINT '  /* for oracle */
-         -- || ' ADD CONSTRAINT '  /* for postgresql */
-         || SUBSTR (   REPLACE (SUBSTR (TO_CHAR(columnname), 1, LENGTH (columnname) - 3),
+         -- || ' ADD (CONSTRAINT '  /* for oracle */
+         || ' ADD CONSTRAINT '  /* for postgresql */
+         || SUBSTR (   REPLACE (SUBSTR (columnname, 1, LENGTH (columnname) - 3),
                                 '_',
                                 ''
                                )
@@ -25,16 +24,16 @@ SELECT      'ALTER TABLE '
          || columnname
          || ') REFERENCES '
          || table_ref
-         || ');'||CHR(10) cmd  /* for oracle */
-    -- || ' DEFERRABLE INITIALLY DEFERRED;'||chr(10) cmd  /* for postgresql */
+         -- || ');'||CHR(10) AS cmd  /* for oracle */
+         || ' DEFERRABLE INITIALLY DEFERRED;'||chr(10) AS cmd  /* for postgresql */
 FROM     (
 -- Table Direct or Search Table Direct
           SELECT t.tablename, c.columnname, r.NAME, c.ad_reference_id,
                  c.ad_reference_value_id,
-                 CAST (SUBSTR (columnname, 1, LENGTH (columnname) - 3) AS VARCHAR2(40)) table_ref
+                 CAST (SUBSTR (columnname, 1, LENGTH (columnname) - 3) AS VARCHAR(40)) AS table_ref
             FROM AD_TABLE t, AD_COLUMN c, AD_REFERENCE r
            WHERE t.ad_table_id = c.ad_table_id
---           AND t.tablename LIKE 'ASP\_%' ESCAPE '\'
+             -- AND t.tablename LIKE 'ASP|_%' ESCAPE '|'
              AND c.columnsql IS NULL
              AND c.ad_reference_id = r.ad_reference_id
              AND (   c.ad_reference_id IN (19 /*Table Direct*/)
@@ -52,7 +51,7 @@ FROM     (
                  AD_REF_TABLE rt,
                  AD_TABLE tr
            WHERE t.ad_table_id = c.ad_table_id
---           AND t.tablename LIKE 'ASP\_%' ESCAPE '\'
+             -- AND t.tablename LIKE 'ASP|_%' ESCAPE '|'
              AND c.columnsql IS NULL
              AND c.ad_reference_id = r.ad_reference_id
              AND (   c.ad_reference_id IN (18 /*Table*/)
@@ -64,25 +63,40 @@ FROM     (
              AND rt.ad_table_id = tr.ad_table_id) v
    WHERE v.columnname NOT IN
                       ('AD_Client_ID', 'AD_Org_ID', 'CreatedBy', 'UpdatedBy')
-     AND v.tablename NOT LIKE 'T\_%' ESCAPE '\'
+     AND v.tablename NOT LIKE 'T|_%' ESCAPE '|'
      AND v.tablename NOT IN ('Test')
      -- exclude views
      AND NOT EXISTS (
                SELECT 1
-                 FROM user_objects
-                WHERE object_name = UPPER (v.tablename)
-                  AND object_type = 'VIEW')
+                 FROM pg_views
+                WHERE viewname = LOWER (v.tablename))
      -- exclude already existing foreign keys
      AND NOT EXISTS (
-            SELECT 1
-              FROM user_constraints c,
-                   user_cons_columns cc,
-                   user_constraints ck
-             WHERE c.table_name = UPPER (v.tablename)
-               AND cc.column_name = UPPER (v.columnname)
-               AND c.constraint_name = cc.constraint_name
-               AND c.r_constraint_name = ck.constraint_name
-               AND ck.constraint_type = 'P'
-               AND c.constraint_type = 'R'
-               AND ck.table_name = UPPER (v.table_ref))
+	SELECT 1
+	FROM pg_constraint cop 
+			LEFT JOIN pg_class conclp 
+			ON (conclp.relnamespace = cop.connamespace AND
+			conclp.relname = cop.conname) 
+			LEFT JOIN pg_tablespace tsp 
+			ON (conclp.reltablespace = tsp.oid), pg_class clp, pg_namespace nsp , 
+			pg_constraint cof 
+			LEFT JOIN pg_class conclf 
+			ON (conclf.relnamespace = cof.connamespace AND
+			conclf.relname = cof.conname) 
+			LEFT JOIN pg_tablespace tsf 
+			ON (conclf.reltablespace = tsf.oid) 
+			JOIN pg_attribute af 
+			ON (af.attrelid = cof.conrelid), pg_class clf, pg_namespace nsf 
+	WHERE cop.conrelid = clp.oid AND
+		clp.relnamespace = nsp.oid AND
+		clp.relname = LOWER (v.table_ref) AND
+		nsp.nspname = 'adempiere' AND
+		cop.contype = 'p' AND
+		cof.conrelid = clf.oid AND
+		clf.relnamespace = nsf.oid AND
+		clf.relname = LOWER (v.tablename) AND
+		nsf.nspname = 'adempiere' AND
+		cof.contype = 'f' AND
+		af.attnum = cof.conkey[1] AND
+		af.attname = LOWER (v.columnname))
 ORDER BY v.tablename, v.table_ref, v.columnname
