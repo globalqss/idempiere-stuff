212ef032506741e40db98ec13f7b4ebebbf52167
globalqss <globalqss@localhost>
2012-09-24 21:19:56 -0500

    Improvements to generate wiki index scripts (postgresql version too)

--- genwikiindex/QSS_GET_TABLES_FROM_WINDOW_pg.sql
+++ genwikiindex/QSS_GET_TABLES_FROM_WINDOW_pg.sql
@@ -1,19 +1,21 @@
-CREATE OR REPLACE FUNCTION qss_get_tables_from_window (p_ad_window_id IN NUMBER)
-   RETURN VARCHAR2
+CREATE OR REPLACE FUNCTION qss_get_tables_from_window (p_ad_window_id IN NUMERIC)
+   RETURNS VARCHAR
 AS
-   v_tables   VARCHAR2 (2000);
+$BODY$
+DECLARE
+   v_tables   VARCHAR (2000);
+   r RECORD;
+   j RECORD;
 BEGIN
-   v_tables := RPAD (' ', 2000, ' ');
    v_tables := '';
-   FOR r IN (SELECT   ad_tab_id,
-                      RPAD ('+', (tablevel), '+') || tablename tablename
+   FOR r IN SELECT   ad_tab_id,
+                      RPAD ('+', (tablevel::integer), '+') || tablename tablename
                  FROM ad_tab b, ad_table t
                 WHERE b.ad_table_id = t.ad_table_id
                   AND b.ad_window_id = p_ad_window_id
-             ORDER BY seqno)
-   LOOP
+             ORDER BY seqno LOOP
       v_tables := v_tables || ' ' || r.tablename;
-      FOR j IN (SELECT NVL (p.classname, p.procedurename) cmd
+      FOR j IN (SELECT COALESCE (p.classname, p.procedurename) cmd
                   FROM ad_field f, ad_column c, ad_process p
                  WHERE f.ad_tab_id = r.ad_tab_id
                    AND f.ad_column_id = c.ad_column_id
@@ -28,4 +30,5 @@ BEGIN
    END LOOP;
    RETURN SUBSTR (v_tables, 2);
 END;
-/
+$BODY$
+LANGUAGE 'plpgsql'
