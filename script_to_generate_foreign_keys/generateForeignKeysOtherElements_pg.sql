219603a6806750b8438c1c2524e1ef0374678dd1
Carlos Ruiz <carg67@gmail.com>
2013-08-23 09:59:12 -0500

    IDEMPIERE-1157 Adding functionality to add charts on the standard windows

--- script_to_generate_foreign_keys/generateForeignKeysOtherElements_pg.sql
+++ script_to_generate_foreign_keys/generateForeignKeysOtherElements_pg.sql
@@ -250,4 +250,47 @@ and t.isactive='Y' AND c.isactive='Y' and c.columnsql IS NULL and t.isview = 'N'
 		af.attnum = cof.conkey[1] AND
 		af.attname = LOWER (c.columnname))
 ) as internal
+
+union
+
+-- 53370 - Chart - AD_Chart
+select 'ALTER TABLE '||tablename||
+' ADD CONSTRAINT '||SUBSTR (REPLACE (SUBSTR (columnname, 1, LENGTH (columnname) - 3), '_', '') || '_' || REPLACE (tablename, '_', ''), 1, 30)||
+' FOREIGN KEY('||columnname||') REFERENCES AD_Chart(AD_Chart_ID)'
+||' DEFERRABLE INITIALLY DEFERRED'||
+';'||chr(10)
+from (
+select t.tablename, c.columnname, c.ad_reference_id, c.ad_reference_value_id from ad_table t join ad_column c on (t.ad_table_id=c.ad_table_id) 
+where c.ad_reference_id = 53370
+and t.isactive='Y' AND c.isactive='Y' and c.columnsql IS NULL and t.isview = 'N' and t.tablename not like 'T|_%' escape '|' and t.tablename not in ('Test')
+     AND NOT EXISTS (
+	SELECT 1
+	FROM pg_constraint cop 
+			LEFT JOIN pg_class conclp 
+			ON (conclp.relnamespace = cop.connamespace AND
+			conclp.relname = cop.conname) 
+			LEFT JOIN pg_tablespace tsp 
+			ON (conclp.reltablespace = tsp.oid), pg_class clp, pg_namespace nsp , 
+			pg_constraint cof 
+			LEFT JOIN pg_class conclf 
+			ON (conclf.relnamespace = cof.connamespace AND
+			conclf.relname = cof.conname) 
+			LEFT JOIN pg_tablespace tsf 
+			ON (conclf.reltablespace = tsf.oid) 
+			JOIN pg_attribute af 
+			ON (af.attrelid = cof.conrelid), pg_class clf, pg_namespace nsf 
+	WHERE cop.conrelid = clp.oid AND
+		clp.relnamespace = nsp.oid AND
+		clp.relname = LOWER ('AD_Chart') AND
+		nsp.nspname = 'adempiere' AND
+		cop.contype = 'p' AND
+		cof.conrelid = clf.oid AND
+		clf.relnamespace = nsf.oid AND
+		clf.relname = LOWER (t.tablename) AND
+		nsf.nspname = 'adempiere' AND
+		cof.contype = 'f' AND
+		af.attnum = cof.conkey[1] AND
+		af.attname = LOWER (c.columnname))
+) as internal
+
 order by 1
