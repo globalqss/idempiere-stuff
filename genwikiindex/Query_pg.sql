c547248424ca41228ceed6b194737ce8aaa4f0a6
globalqss <globalqss@localhost>
2013-10-18 14:58:36 -0500

    wiki index generator

--- genwikiindex/Query_pg.sql
+++ genwikiindex/Query_pg.sql
@@ -1,9 +1,11 @@
 SELECT     t.node_id, t.parent_id, t.depth, t.seqno,
-           issummary, rpad('>', depth-1, '>') || m.NAME as name,
+           issummary,
+	   -- rpad('>', depth-1, '>') || 
+	   replace(m.name, ',', '|') as name,
                  COALESCE(m.action, '') AS TYPE,
                  CASE m.action
                          WHEN 'B' THEN 'Workbench'
-                         WHEN 'F' THEN 'WorkFlow'
+                         WHEN 'F' THEN 'Workflow'
                          WHEN 'P' THEN 'Process'
                          WHEN 'R' THEN 'Report'
                          WHEN 'T' THEN 'Task'
@@ -21,6 +23,7 @@ SELECT     t.node_id, t.parent_id, t.depth, t.seqno,
                               )
                          )
                     ) AS technical,
+                 coalesce(p.ad_process_id, f.ad_form_id, k.ad_task_id, w.ad_window_id, wf.ad_workflow_id) as id,
                  COALESCE(COALESCE(f.isbetafunctionality, COALESCE(p.isbetafunctionality, COALESCE(wf.isbetafunctionality, w.isbetafunctionality), ''))) as isbetafunctionality,
                   t.path
             FROM (
@@ -32,11 +35,11 @@ select t.node_id, t.parent_id, fm.depth+1, t.seqno, path || t.node_id::varchar f
 select * from fullmenu) 
 as t
                  JOIN AD_MENU m ON t.node_id = m.ad_menu_id
-                 LEFT JOIN AD_PROCESS p ON m.ad_process_id = p.ad_process_id
-                 LEFT JOIN AD_FORM f ON m.ad_form_id = f.ad_form_id
-                 LEFT JOIN AD_TASK k ON m.ad_task_id = k.ad_task_id
-                 LEFT JOIN AD_WINDOW w ON m.ad_window_id = w.ad_window_id
-                 LEFT JOIN AD_WORKFLOW wf ON m.ad_workflow_id = wf.ad_workflow_id
+                 LEFT JOIN AD_PROCESS p ON m.ad_process_id = p.ad_process_id AND p.isactive='Y'
+                 LEFT JOIN AD_FORM f ON m.ad_form_id = f.ad_form_id AND f.isactive='Y'
+                 LEFT JOIN AD_TASK k ON m.ad_task_id = k.ad_task_id AND k.isactive='Y'
+                 LEFT JOIN AD_WINDOW w ON m.ad_window_id = w.ad_window_id AND w.isactive='Y'
+                 LEFT JOIN AD_WORKFLOW wf ON m.ad_workflow_id = wf.ad_workflow_id AND wf.isactive='Y'
            WHERE m.isactive = 'Y'
              -- AND m.ad_menu_id < 1000000
 order by t.path
