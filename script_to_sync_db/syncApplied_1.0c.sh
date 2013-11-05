1c666366062c05804c95cdccef4dc0d0c69eefea
Carlos Ruiz <carg67@gmail.com>
2013-11-05 15:08:30 -0500

    Backed out changeset e5f1888fa6f6

--- script_to_sync_db/syncApplied_1.0c.sh
+++ script_to_sync_db/syncApplied_1.0c.sh
@@ -1,56 +1,54 @@
-DATABASE=${1:-idempiere}
+#
+# Contributor: Carlos Ruiz - globalqss
+# June 27, 2013
+# Script to synchronize your 1.0c installation with latest migration scripts
+# it's a tool to execute in sync with the p2 update
+#   sh update.sh http://ci.idempiere.org/job/iDempiere1.0c/ws/buckminster.output/org.adempiere.server_1.0.0-eclipse.feature/site.p2
+#
+FOLDER=i1.0c-release
+DATABASE=idempiere
 USER=adempiere
-ADDPG=${2}   # i.e. "-h localhost -p 5432"
+JENKINSURL=http://ci.idempiere.org/job/iDempiere1.0c
 
-MIGRATIONDIR=~/hgAdempiere/localosgi/migration
-cd $MIGRATIONDIR
+BASEDIR=`dirname $0`
+cd $BASEDIR
+wget -O ${FOLDER}_pg.zip "${JENKINSURL}/ws/migration/${FOLDER}/postgresql/*zip*/postgresql.zip"
+wget -O post_pg.zip      "${JENKINSURL}/ws/migration/processes_post_migration/postgresql/*zip*/postgresql.zip"
+rm -rf ${FOLDER}_pg
+mkdir -p ${FOLDER}_pg
+unzip -u -d ${FOLDER}_pg ${FOLDER}_pg.zip
+mkdir -p post_pg
+unzip -u -d post_pg post_pg.zip
 
-psql -d $DATABASE -U $USER $ADDPG -q -t -c "select name from ad_migrationscript" | sed -e 's:^ ::' | grep -v '^$' | sort > /tmp/lisDB.txt
-
-> /tmp/lisFS.txt
-for FOLDER in i1.0c i2.0
-do
-    if [ -d ${FOLDER}/postgresql ]
-    then
-        cd ${FOLDER}/postgresql
-        ls *.sql | sort >> /tmp/lisFS.txt
-        cd ../..
-    fi
-done
-sort -o /tmp/lisFS.txt /tmp/lisFS.txt
-sort -o /tmp/lisDB.txt /tmp/lisDB.txt
-
+psql -d $DATABASE -U $USER -q -t -c "select name from ad_migrationscript" | sed -e 's:^ ::' | grep -v '^$' | sort > /tmp/lisDB.txt
+cd ${FOLDER}_pg/postgresql
+ls *.sql | sort > /tmp/lisFS.txt
 MSGERROR=""
 APPLIED=N
 for i in `comm -13 /tmp/lisDB.txt /tmp/lisFS.txt`
 do
-    SCRIPT=`find . -name "$i" -print | fgrep -v /oracle/`
     OUTFILE=/tmp/`basename "$i" .sql`.out
-    psql -d $DATABASE -U $USER $ADDPG -f "$SCRIPT" 2>&1 | tee "$OUTFILE"
+    psql -d $DATABASE -U $USER -f "$i" 2>&1 | tee "$OUTFILE"
     if fgrep ERROR: "$OUTFILE" > /dev/null 2>&1
     then
-        MSGERROR="$MSGERROR
-**** ERROR ON FILE $OUTFILE - Please verify ****"
+        MSGERROR="$MSGERROR\n**** ERROR ON FILE $OUTFILE - Please verify ****"
     fi
     APPLIED=Y
 done
 if [ x$APPLIED = xY ]
 then
-    for i in processes_post_migration/postgresql/*.sql
+    cd ../..
+    for i in post_pg/postgresql/*.sql
     do
         OUTFILE=/tmp/`basename "$i" .sql`.out
-        psql -d $DATABASE -U $USER $ADDPG -f "$i" 2>&1 | tee "$OUTFILE"
+        psql -d $DATABASE -U $USER -f "$i" 2>&1 | tee "$OUTFILE"
         if fgrep ERROR: "$OUTFILE" > /dev/null 2>&1
         then
-            MSGERROR="$MSGERROR
-**** ERROR ON FILE $OUTFILE - Please verify ****"
+            MSGERROR="$MSGERROR\n**** ERROR ON FILE $OUTFILE - Please verify ****"
         fi
     done
-else
-    echo "Database is up to date, no scripts to apply"
 fi
 if [ -n "$MSGERROR" ]
 then
     echo "$MSGERROR"
 fi
-# checkApplied.sh
