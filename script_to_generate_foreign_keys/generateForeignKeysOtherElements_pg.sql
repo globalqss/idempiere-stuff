950536a30ffa97e20b6bc501b49e081c9d8be10d
Carlos Ruiz <carg67@gmail.com>
2019-06-17 19:49:49 +0200

    Simplify script

--- script_to_generate_foreign_keys/generateForeignKeysOtherElements_pg.sql
+++ script_to_generate_foreign_keys/generateForeignKeysOtherElements_pg.sql
@@ -1,83 +1,24 @@
--- 25 - Account - C_ValidCombination
--- columns expected to end with _Acct
 select 'ALTER TABLE '||tablename||
-' ADD CONSTRAINT '||SUBSTR (REPLACE (SUBSTR (columnname, 1, LENGTH (columnname) - 5), '_', '') || 'vc_' || REPLACE (tablename, '_', ''), 1, 30)||
-' FOREIGN KEY('||columnname||') REFERENCES C_ValidCombination(C_ValidCombination_ID)'
+' ADD CONSTRAINT '||SUBSTR (REPLACE (SUBSTR (columnname, 1, LENGTH (columnname) - CASE WHEN table_ref='C_ValidCombination' THEN 5 ELSE 3 END), '_', '') || CASE WHEN table_ref='C_ValidCombination' THEN 'vc_' ELSE '_' END || REPLACE (tablename, '_', ''), 1, 30)||
+' FOREIGN KEY('||columnname||') REFERENCES '||table_ref||'('||table_ref||'_ID)'
 ||' DEFERRABLE INITIALLY DEFERRED'||
 ';'||chr(10) as cmd from (
 --
 SELECT * FROM (
---
-select tablename, columnname, 'C_ValidCombination' AS table_ref
-from (
-select t.tablename, c.columnname, c.ad_reference_id, c.ad_reference_value_id from ad_table t join ad_column c on (t.ad_table_id=c.ad_table_id) 
-where c.ad_reference_id = 25
-and t.isactive='Y' AND c.isactive='Y' and c.columnsql IS NULL and t.isview = 'N' and t.tablename not like 'T|_%' escape '|' and t.tablename not in ('Test')
+SELECT t.tablename, c.columnname, 
+CASE ad_reference_id
+  WHEN 21 THEN 'C_Location'
+  WHEN 25 THEN 'C_ValidCombination'
+  WHEN 31 THEN 'M_Locator'
+  WHEN 32 THEN 'AD_Image'
+  WHEN 33 THEN 'S_ResourceAssignment'
+  WHEN 35 THEN 'M_AttributeSetInstance'
+  WHEN 53370 THEN 'AD_Chart'
+END AS table_ref
+from ad_table t join ad_column c on (t.ad_table_id=c.ad_table_id) 
+where c.ad_reference_id IN (21, 25, 31, 32, 33, 35, 53370)
+and t.isactive='Y' AND c.isactive='Y' and c.columnsql IS NULL and t.isview = 'N'
 ) as internal
---
-union
---
--- 33 - Assignment - S_ResourceAssignment
--- columns expected to end with _ID - all below
-select tablename, columnname, 'S_ResourceAssignment' AS table_ref
-from (
-select t.tablename, c.columnname, c.ad_reference_id, c.ad_reference_value_id from ad_table t join ad_column c on (t.ad_table_id=c.ad_table_id) 
-where c.ad_reference_id = 33
-and t.isactive='Y' AND c.isactive='Y' and c.columnsql IS NULL and t.isview = 'N' and t.tablename not like 'T|_%' escape '|' and t.tablename not in ('Test')
-) as internal
---
-union
---
--- 32 - Image - AD_Image
-select tablename, columnname, 'AD_Image' AS table_ref
-from (
-select t.tablename, c.columnname, c.ad_reference_id, c.ad_reference_value_id from ad_table t join ad_column c on (t.ad_table_id=c.ad_table_id) 
-where c.ad_reference_id = 32
-and t.isactive='Y' AND c.isactive='Y' and c.columnsql IS NULL and t.isview = 'N' and t.tablename not like 'T|_%' escape '|' and t.tablename not in ('Test')
-and columnname != 'BinaryData'
-) as internal
---
-union
---
--- 21 - Location - C_Location
-select tablename, columnname, 'C_Location' AS table_ref
-from (
-select t.tablename, c.columnname, c.ad_reference_id, c.ad_reference_value_id from ad_table t join ad_column c on (t.ad_table_id=c.ad_table_id) 
-where c.ad_reference_id = 21
-and t.isactive='Y' AND c.isactive='Y' and c.columnsql IS NULL and t.isview = 'N' and t.tablename not like 'T|_%' escape '|' and t.tablename not in ('Test')
-) as internal
---
-union
---
--- 31 - Locator - M_Locator
-select tablename, columnname, 'M_Locator' AS table_ref
-from (
-select t.tablename, c.columnname, c.ad_reference_id, c.ad_reference_value_id from ad_table t join ad_column c on (t.ad_table_id=c.ad_table_id) 
-where c.ad_reference_id = 31
-and t.isactive='Y' AND c.isactive='Y' and c.columnsql IS NULL and t.isview = 'N' and t.tablename not like 'T|_%' escape '|' and t.tablename not in ('Test')
-) as internal
---
-union
---
--- 35 - Product Attribute - M_AttributeSetInstance
-select tablename, columnname, 'M_AttributeSetInstance' AS table_ref
-from (
-select t.tablename, c.columnname, c.ad_reference_id, c.ad_reference_value_id from ad_table t join ad_column c on (t.ad_table_id=c.ad_table_id) 
-where c.ad_reference_id = 35
-and t.isactive='Y' AND c.isactive='Y' and c.columnsql IS NULL and t.isview = 'N' and t.tablename not like 'T|_%' escape '|' and t.tablename not in ('Test')
-) as internal
---
-union
---
--- 53370 - Chart - AD_Chart
-select tablename, columnname, 'AD_Chart' AS table_ref
-from (
-select t.tablename, c.columnname, c.ad_reference_id, c.ad_reference_value_id from ad_table t join ad_column c on (t.ad_table_id=c.ad_table_id) 
-where c.ad_reference_id = 53370
-and t.isactive='Y' AND c.isactive='Y' and c.columnsql IS NULL and t.isview = 'N' and t.tablename not like 'T|_%' escape '|' and t.tablename not in ('Test')
-) as internal
---
-) AS colref
 WHERE NOT EXISTS (
 	SELECT 1
 	FROM pg_constraint cop 
@@ -106,6 +47,7 @@ WHERE NOT EXISTS (
 		cof.contype = 'f' AND
 		af.attnum = cof.conkey[1] AND
 		af.attname = LOWER (columnname))
-AND (table_ref='C_ValidCombination' AND tablename NOT IN ('A_Asset_Acct','A_Asset_Change','A_Asset_Group_Acct','A_Asset_Transfer','A_FundingMode_Acct','I_Asset'))
+AND NOT (table_ref='C_ValidCombination' AND tablename IN ('A_Asset_Acct','A_Asset_Change','A_Asset_Group_Acct','A_Asset_Transfer','A_FundingMode_Acct','I_Asset'))
+AND tablename not like 'T|_%' escape '|' and tablename not in ('Test')
 ) as cmds
 order by 1;
